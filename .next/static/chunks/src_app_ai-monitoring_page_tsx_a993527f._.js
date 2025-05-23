5a4e22cdf82730e3d2cdeb1617c950c7ce8a1086
Christudas <christudas@gmail.com>
2025-05-23 11:54:24 +0530

    Add .next/static/chunks/src_app_ai-monitoring_page_tsx_a993527f._.js 1747981463585

---
+++ .next/static/chunks/src_app_ai-monitoring_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2f503249._.js",
+  "static/chunks/src_1159edb4._.js"
+],
+    source: "dynamic"
+});
